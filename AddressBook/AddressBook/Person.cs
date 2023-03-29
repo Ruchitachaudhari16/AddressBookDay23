f80f4c8b2208baf487a290c438ad3cfbe7626af7
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-29 08:43:53 -0400

    [Ruchita] Add: NewContact Added

--- AddressBook/AddressBook/Person.cs
+++ AddressBook/AddressBook/Person.cs
@@ -13,8 +13,8 @@ namespace AddressBook
             public string Address { get; set; }
             public string City { get; set; }
             public string State { get; set; }
-            public static int Zip { get; set; }
-            public static long PhoneNumber { get; set; }
+            public  string Zip{ get; set; }
+            public  string PhoneNumber { get; set; }
             public string Email { get; set; }
 
         }
